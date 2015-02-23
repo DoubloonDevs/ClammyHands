8d434a509c06de4343728d01d0ad62f1fd18e6f5
DoubloonDevs <doubloondevs@hotmail.com>
2015-02-23 18:20:37 +0000

    Update Game.js

--- Game.js
+++ Game.js
@@ -305,7 +305,7 @@ gabeChat.prototype.update = function() {
       c.fillText("Be seeing you soon, ya wee", this.x + 45, this.y - 34);
       c.fillText("scrub.", this.x + 45, this.y - 23);
     }
-  } else if (kills >= 200 && kills < 300 && this.pop_up_count === 0 && !turret_deployed) {
+  } else if (kills >= 50 && kills < 100 && this.pop_up_count === 0 && !turret_deployed) {
     this.collapsed = false;
     if (chat.currentTime === 0) chat.play();
     c.textAlign = 'left';
